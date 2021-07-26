2cad4d4b46b6bd3d06c268ff64e6171a1bfe35eb
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-07-26 11:56:29 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  let [middleDate, setMiddleDate] = useState(new Date().getTime())
+
+  function getDate(position) {
+      let date = middleDate + ((position - 3) * 8.64e+7)
+      return new Date(date).toDateString()
+  }
+
+  let dates = new Array(7).fill(null).map((el, i) => getDate(i))
+
+  return(
+      <div className="container">
+          <button className="prev" onClick={() => setMiddleDate(p => p - ((8.64e+7) * 7))}>●</button>
+          {dates.map(el => <div className={el === new Date().toDateString() ? "current date" : "date"}>{el.substr(4, 6)}</div>)}
+          <button className="next" onClick={() => setMiddleDate(p => p + ((8.64e+7) * 7))}>●</button>
+      </div>
+  )
 }
-
-export default App;
